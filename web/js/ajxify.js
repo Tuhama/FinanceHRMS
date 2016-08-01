f5fdd75e219e077e0fee6966e29b7dc75382c373
TUHAMA <TUHAMA@Tupc>
2016-08-01 13:51:34 +0200

    new1

--- web/js/ajxify.js
+++ web/js/ajxify.js
@@ -2,54 +2,101 @@
 var req;
 var isIE;
 var empId = "";
+var currentDataTable;//to reach the curently edited data table
 
 function overrideSubmits() {
     $("form#personalInfoForm").submit(function (event) {
         EmpInfo("addEmployee", $("form#personalInfoForm").serialize());
         event.preventDefault();
     });
-        $("form#personalInfoEditForm").submit(function (event) {
+    $("form#personalInfoEditForm").submit(function (event) {
         EmpInfo("editEmployee", $("form#personalInfoEditForm").serialize());
         event.preventDefault();
     });
+
+
     $("form#empEventForm").submit(function (event) {
         createEmpDetail("addEvent", $("form#empEventForm").serialize());
         event.preventDefault();
+        currentDataTable = $('#addEvent_table');
+    });
+    $("form#deleteEvent_form").submit(function (event) {
+        deleteEmpDetail("deleteEvent", $("form#deleteEvent_form").serialize());
+        event.preventDefault();
+        currentDataTable = $('#addEvent_table');
     });
 
+
     $("form#unpaidVForm").submit(function (event) {
-        createEmpDetail("addUnpaidVacation", $("form#unpaidVForm").serialize());
+        createEmpDetail("addUnpaidV", $("form#unpaidVForm").serialize());
         event.preventDefault();
+        currentDataTable = $('#addUnpaidVacation_table');
+    });
+    $("form#deleteUnpaidV_form").submit(function (event) {
+        deleteEmpDetail("deleteUnpaidV", $("form#deleteUnpaidV_form").serialize());
+        event.preventDefault();
+        currentDataTable = $('#addUnpaidV_table');
     });
 
     $("form#serviceJoinForm").submit(function (event) {
         createEmpDetail("addServiceJoin", $("form#serviceJoinForm").serialize());
         event.preventDefault();
+        currentDataTable = $('#addServiceJoin_table');
+    });
+    $("form#deleteServiceJoin_form").submit(function (event) {
+        deleteEmpDetail("deleteUnpaidV", $("form#deleteServiceJoin_form").serialize());
+        event.preventDefault();
+        currentDataTable = $('#addServiceJoin_table');
     });
 
     $("form#healthLeavForm").submit(function (event) {
         createEmpDetail("addHealthLeave", $("form#healthLeavForm").serialize());
         event.preventDefault();
+        currentDataTable = $('#addHealthLeave_table');
+    });
+    $("form#deleteHealthLeave_form").submit(function (event) {
+        deleteEmpDetail("deleteHealthLeave", $("form#deleteHealthLeave_form").serialize());
+        event.preventDefault();
+        currentDataTable = $('#addHealthLeave_table');
     });
 
     $("form#trainingForm").submit(function (event) {
-        createEmpDetail("addTrainingCourse", $("form#trainingForm").serialize());
+        createEmpDetail("addTraining", $("form#trainingForm").serialize());
         event.preventDefault();
+        currentDataTable = $('#addTraining_table');
     });
+    $("form#deleteTraining_form").submit(function (event) {
+        deleteEmpDetail("deleteTraining", $("form#deleteTraining_form").serialize());
+        event.preventDefault();
+        currentDataTable = $('#addTraining_table');
+    });
+
     $("form#rewardForm").submit(function (event) {
         createEmpDetail("addReward", $("form#rewardForm").serialize());
         event.preventDefault();
+        currentDataTable = $('addReward_table');
+    });
+    $("form#deleteReward_form").submit(function (event) {
+        deleteEmpDetail("deleteReward", $("form#deleteReward_form").serialize());
+        event.preventDefault();
+        currentDataTable = $('#addReward_table');
     });
 
     $("form#punishmentForm").submit(function (event) {
         createEmpDetail("addPunishment", $("form#punishmentForm").serialize());
         event.preventDefault();
+        currentDataTable = $('#addPunishment_table');
+    });
+    $("form#deletePunishment_form").submit(function (event) {
+        deleteEmpDetail("deletePunishment", $("form#deletePunishment_form").serialize());
+        event.preventDefault();
+        currentDataTable = $('#addPunishment_table');
     });
 }
-    
+
 function initRequest() {
     if (window.XMLHttpRequest) {
-        if (navigator.userAgent.indexOf('MSIE') != -1) {
+        if (navigator.userAgent.indexOf('MSIE') !== -1) {
             isIE = true;
         }
         return new XMLHttpRequest();
@@ -69,8 +116,8 @@ function EmpInfo(uri, form_data) {
 }
 
 function callbackEmp() {
-    if (req.readyState == 4) {
-        if (req.status == 200) {
+    if (req.readyState === 4) {
+        if (req.status === 200) {
             empId = req.responseText;
             alert("تم الادخال بنجاح");
         }
@@ -78,13 +125,11 @@ function callbackEmp() {
 }
 
 function createEmpDetail(uri, form_data) {
-    if (empId != "") {
-        var url = uri + "?" + form_data +"&currentEmp="+empId;
+    if (empId !== "") {
+        var url = uri + "?" + form_data + "&currentEmp=" + empId;
         req = initRequest();
         req.open("POST", url, true);
-
         req.onreadystatechange = callbackDetail;
-
         req.send(null);
     }
     else
@@ -95,18 +140,32 @@ function createEmpDetail(uri, form_data) {
     }
 }
 function callbackDetail() {
-
-    if (req.readyState == 4) {
-        if (req.status == 200) {
-            processCallback(req.responseText);
-            alert("تم الادخال بنجاح");           
+    if (req.readyState === 4) {
+        if (req.status === 200) {
+            currentDataTable.DataTable().row.add($.parseJSON(req.responseText)).draw(false);
+            ///this didn't work  var is not obj it seems!!      >>>  $('#addEvent_table').DataTable().row.add( responseJson ).draw(false);
+            alert("تم الادخال بنجاح");
         }
+        else
+            alert("خطأ في الادخال .." + req.status);
     }
+
 }
 
-function processCallback(responseJson){
-
-    $('#addEvent_table').DataTable().row.add($.parseJSON(  responseJson) ).draw(false);
-    ///this didn't work  var is not obj it seems!!      >>>  $('#addEvent_table').DataTable().row.add( responseJson ).draw(false);
+function deleteEmpDetail(uri, form_data) {
+    var url = uri + "?" + form_data;
+    req = initRequest();
+    req.open("POST", url, true);
+    req.onreadystatechange = callbackDeleteDetail;
+    req.send(null);
+}
+function callbackDeleteDetail() {
+    if (req.readyState === 4) {
+        if (req.status === 200) {
+            currentDataTable.DataTable().row('.selected').remove().draw(false);
+        }
+        else
+            alert("خطأ في الادخال .." + req.status);
+    }
 
 }
